0199ad9eabce68b27f12387957895a5d868c833a
Saibal De <saibal.de.bnk@gmail.com>
2023-05-26 15:58:20 -0700

    Set up neovim fuzzy search, lsp and completion

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,11 +17,27 @@ require('packer').startup(function(use)
     use 'Shatur/neovim-ayu'
 
     use 'nvim-tree/nvim-tree.lua'
+
     use 'nvim-lualine/lualine.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
+        requires = {
+            'nvim-lua/plenary.nvim'
+        }
+    }
 
     use 'nvim-treesitter/nvim-treesitter'
 
-    use 'lervag/vimtex'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+
+    use 'L3MON4D3/LuaSnip'
+
+    use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
+    use 'neovim/nvim-lspconfig'
 
     if packer_bootstrap then
         require('packer').sync()
@@ -72,19 +88,102 @@ require('nvim-tree').setup({
 })
 vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>')
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
 require('nvim-treesitter.configs').setup({
     ensure_installed = {
         'c',
         'cpp',
+        'cmake',
         'python',
-        'lua',
-        'vim'
+        'lua'
     },
     sync_install = false,
     auto_install = false,
     highlight = {
         enable = true
     }
+})
+
+local luasnip = require('luasnip')
+local cmp = require('cmp')
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end
+    },
+    widnow = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered()
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-l>'] = cmp.mapping.complete(),
+        ['<C-h>'] = cmp.mapping.abort()
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    }, {
+        { name = 'buffer' }
+    })
+})
+
+require('mason').setup()
+
+require('mason-lspconfig').setup({
+    ensure_installed = {
+        'clangd',
+        'cmake',
+        'jedi_language_server',
+        'lua_ls'
+    }
+})
+
+local on_attach = function(_, _)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+end
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local lspconfig = require('lspconfig')
+lspconfig['clangd'].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+lspconfig['cmake'].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+lspconfig['jedi_language_server'].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+})
+lspconfig['lua_ls'].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
 
 vim.opt.encoding = 'utf-8'
@@ -100,3 +199,6 @@ vim.opt.smartindent = true
 vim.opt.number = true
 vim.opt.numberwidth = 6
 vim.opt.signcolumn = 'number'
+
+vim.opt.breakindent = true
+vim.opt.linebreak = true
